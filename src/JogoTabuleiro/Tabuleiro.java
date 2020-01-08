c51288bc0af10cb9c8e1827de25f507d3b14cc72
max well <maxii.raamos.carol@gmail.com>
2020-01-08 13:50:00 -0300

    Implementando a classe TabubleiroException e programação defensiva

--- src/JogoTabuleiro/Tabuleiro.java
+++ src/JogoTabuleiro/Tabuleiro.java
@@ -6,6 +6,14 @@ private int coluna;
 private Peca[][] pecas;
 public Tabuleiro(int linha, int coluna) {
 	
+	if(linha < 1 || coluna < 1) {
+	throw new TabuleiroException("Erro Criando Tabuleiro:  é necessario que haja pelo menos 1 linha e 1 coluna.! ");
+	
+		
+	}
+	
+	
+	
 	this.linha = linha;
 	this.coluna = coluna;
 	pecas = new Peca[linha][coluna];
@@ -13,27 +21,51 @@ public Tabuleiro(int linha, int coluna) {
 public int getLinha() {
 	return linha;
 }
-public void setLinha(int linha) {
-	this.linha = linha;
-}
+
 public int getColuna() {
 	return coluna;
 }
-public void setColuna(int coluna) {
-	this.coluna = coluna;
-}
+
 
 public Peca peca(int linha, int coluna) {
+	if(!posicaoExistente(linha,coluna)) {
+		throw new TabuleiroException(" Posição não existe");
+		
+	}
 	return pecas[linha][coluna];
 }
 
 public Peca peca(Posicao posicao) {
+	if(!posicaoExistente(posicao)) {
+		throw new TabuleiroException(" Posição não existe");
+		
+	}
 	return pecas[posicao.getLinha()][posicao.getColuna()];
 }
 public void  colocarPeca(Peca peca,Posicao posicao) {
-	
+	if(haPeca(posicao)) {
+		throw new TabuleiroException("Já existe uma peça na posicao."+ posicao);
+	}
 	pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 	peca.posicao = posicao;
 }
+
+private boolean posicaoExistente(int Linha,int Coluna) {
+	return Linha >=0&&Linha<linha &&Coluna>=0&&Coluna<coluna;
+}
+
+public boolean posicaoExistente(Posicao posicao) {
+	return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+}
+public boolean haPeca(Posicao posicao) {
+	if(!posicaoExistente(posicao)) {
+		throw new TabuleiroException(" Posição não existe");
+		
+	}	
+return 	peca(posicao)!=null;
+	
+}
+
+
 
 }
